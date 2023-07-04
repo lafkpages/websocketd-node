45b410f32c053bb972fb6137376962c61f3cf585
lafkpages <soy.lafk+gh@gmail.com>
2023-07-04 23:44:20 +0200

    ✨ Run command on connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { WebSocketServer } from "ws";
 import { parseArgs } from "util";
+import { spawn } from "child_process";
 
 const args = parseArgs({
   options: {
@@ -10,10 +11,15 @@ const args = parseArgs({
       short: "p",
     },
   },
+  allowPositionals: true,
 });
 
 const wss = new WebSocketServer({
   port: parseInt(args.values.port) || 3030,
 });
 
-wss.on("connection", (ws) => {});
+wss.on("connection", (ws) => {
+  const child = spawn(args.positionals, {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+});
